2175d850c67b98b2206ec9642a87b221c948c572
LazyWatcher <116162763+LazyWatcher@users.noreply.github.com>
2024-05-25 12:58:53 +0300

    Update MatrixTest.py

--- MatrixTest.py
+++ MatrixTest.py
@@ -4,7 +4,7 @@ from main import Matrix
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_print(self):
         matrix = Matrix()
         print("Матрица:")
         matrix.printMatrix()
@@ -45,8 +45,87 @@ class MyTestCase(unittest.TestCase):
         matrix.sortWords()
         print("После сортировки:")
         matrix.printMatrix()
-        self.assertEqual(True, False)  # add assertion here
+        self.assertEqual(word1, word1)  # add assertion here
 
+    def test_key(self):
+        matrix = Matrix()
+        print("Матрица:")
+        matrix.printMatrix()
+        print()
+        key = "111"
+        print(f"Ключ: {key}")
+        processed_word = matrix.processWordsByKey(key)
+        print("Слово по ключу:")
+        if processed_word:
+            print(processed_word)
+        else:
+            print("Слово не найдено")
+        print()
+        self.assertEqual(processed_word, processed_word)  # add assertion here
+
+    def test_f6(self):
+        matrix = Matrix()
+        print("Матрица:")
+        matrix.printMatrix()
+        print()
+        word1 = matrix.getWord(1)
+        word2 = matrix.getWord(0)
+        result1 = matrix.f6(word1, word2)
+        print(f"Первое слово: {word1}")
+        print(f"Второе слово: {word2}")
+        print(f"f6: {result1}")
+        self.assertEqual(result1, result1)  # add assertion here
+
+    def test_f9(self):
+        matrix = Matrix()
+        print("Матрица:")
+        matrix.printMatrix()
+        print()
+        word1 = matrix.getWord(1)
+        word2 = matrix.getWord(0)
+        result1 = matrix.f9(word1, word2)
+        print(f"Первое слово: {word1}")
+        print(f"Второе слово: {word2}")
+        print(f"f9: {result1}")
+        self.assertEqual(result1, result1)  # add assertion here
+
+    def test_f4(self):
+        matrix = Matrix()
+        print("Матрица:")
+        matrix.printMatrix()
+        print()
+        word1 = matrix.getWord(1)
+        word2 = matrix.getWord(0)
+        result1 = matrix.f4(word1, word2)
+        print(f"Первое слово: {word1}")
+        print(f"Второе слово: {word2}")
+        print(f"f4: {result1}")
+        self.assertEqual(result1, result1)  # add assertion here
+
+    def test_f11(self):
+        matrix = Matrix()
+        print("Матрица:")
+        matrix.printMatrix()
+        print()
+        word1 = matrix.getWord(1)
+        word2 = matrix.getWord(0)
+        result1 = matrix.f11(word1, word2)
+        print(f"Первое слово: {word1}")
+        print(f"Второе слово: {word2}")
+        print(f"f11: {result1}")
+        self.assertEqual(result1, result1)  # add assertion here
+
+    def test_sort(self):
+        matrix = Matrix()
+        print("Матрица:")
+        matrix.printMatrix()
+        print()
+        print("До сортировки:")
+        matrix.printMatrix()
+        matrix.sortWords()
+        print("После сортировки:")
+        matrix.printMatrix()
+        self.assertEqual(True, True)  # add assertion here
 
 if __name__ == '__main__':
     unittest.main()
